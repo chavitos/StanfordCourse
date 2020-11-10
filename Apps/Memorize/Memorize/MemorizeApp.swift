c3b37d4be6486ab503fc9d7ab37c6def0a0701b5
Tiago Chaves <tchaves74@gmail.com>
2020-11-10 10:03:06 -0300

    Lecture 1: Course Logistics and Introduction to SwiftUI
    - Create Memorize app;
    - Create CardView struct;
    - Introduction to HStack, ZStack and ForEach;
    - Text and Shape intro.

---
+++ Apps/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Tiago Chaves on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
